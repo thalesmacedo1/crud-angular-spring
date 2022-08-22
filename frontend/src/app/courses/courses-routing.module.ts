fefd3b79162f1b1008e172c7e26c4567c041534c
Thales Macêdo <thalesmacedo1@live.com>
2022-08-22 12:47:02 -0300

    created courses component and routing

--- frontend/src/app/courses/courses-routing.module.ts
+++ frontend/src/app/courses/courses-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: CoursesComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CoursesRoutingModule { }
+export class CoursesRoutingModule {}
